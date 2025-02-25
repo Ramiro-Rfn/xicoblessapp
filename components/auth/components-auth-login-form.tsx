b0a7120a1cc23c3af33d9a285b7a2c0a0c62993f
Ramiro Nzau <ramirofrancisco962014@gmail.com>
2025-02-25 21:54:05 +0100

    refatoring host

--- components/auth/components-auth-login-form.tsx
+++ components/auth/components-auth-login-form.tsx
@@ -19,7 +19,7 @@ const ComponentsAuthLoginForm = () => {
 
         setIsLoading(true)
 
-        const response = await fetch('http://localhost:3000/api/auth', {
+        const response = await fetch('https://xicoblessapp.vercel.app/api/auth', {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
